f4cd75430598c14734fde771bfbfdef2ff7fad16
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2020-12-01 23:22:45 -0400

    Update db_conexion.php

--- db_conexion.php
+++ db_conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","asustek_db");
+$conex = mysqli_connect("de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","gc46aipzoqx69fqu","adcyit31mv6e0vxh","	hvm5tpp3neofi4nm");
 
 ?>
